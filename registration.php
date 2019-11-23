c4fa7770219090d274320cff21ea2b2bb21800f3
Bandix123 <56293436+Bandix123@users.noreply.github.com>
2019-11-23 22:05:11 +0300

    Create registration.php
    checking with mysql

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+
+$login= $_POST["login"];
+$email= $_POST["email"];
+$pasword= $_POST["password"];
+$password_check= $_POST["check"];
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $sql = "INSERT INTO users (username, email,password)
+  VALUES ('login','email','password')";
+
+  echo "Registred succesfully";
+} else {
+echo "Error";
+}
+
+ ?>
