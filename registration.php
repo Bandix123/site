4f3ed6075001e49024fa095dac9feb1e5b7ade12
Bandix123 <56293436+Bandix123@users.noreply.github.com>
2019-11-23 22:09:22 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -6,11 +6,12 @@ $pasword= $_POST["password"];
 $password_check= $_POST["check"];
 
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  if ($password=$check){
   $sql = "INSERT INTO users (username, email,password)
   VALUES ('login','email','password')";
 
   echo "Registred succesfully";
-} else {
+}} else {
 echo "Error";
 }
 
