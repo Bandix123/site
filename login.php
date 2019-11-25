7cdf465a26a87c14a86ad3d9369051af34f11651
Bandix123 <56293436+Bandix123@users.noreply.github.com>
2019-11-25 21:20:10 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,29 @@
 <?php
-$login= $_POST["login"];
-$email= $_POST["email"];
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-  echo "Thanks for email";
-} else {
-echo "bad email";
-} //check if email is asd@asd.asd
+require('mysqlconnect.php');
+session_start();
 
+if (isset($_POST['username'])){
+        
+ $username = stripslashes($_REQUEST['username']);
 
- ?>
+ $username = mysqli_real_escape_string($con,$username);
+ $password = stripslashes($_REQUEST['password']);
+ $password = mysqli_real_escape_string($con,$password);
+ 
+        $query = "SELECT * FROM `users` WHERE username='$username'
+and password='".md5($password).
+ $result = mysqli_query($con,$query) or die(mysql_error());
+ $rows = mysqli_num_rows($result);
+        if($rows==1){
+     $_SESSION['username'] = $username;
+          
+     header("Location: mainpage.html");
+         }else{
+ echo "<div class='form'>
+<h3>Username/password is incorrect.</h3>
+<br/>Click here to <a href='login.html'>Login</a></div>";
+ }
+}else{
+  header ("Location: login.html");
+}
+?>
