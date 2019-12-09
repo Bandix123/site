c84ce41c696c5aacf2820f3f8767e388a3125d9d
Bandix123 <56293436+Bandix123@users.noreply.github.com>
2019-12-09 19:58:47 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,35 +2,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="registration.css">
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-  <div>
+  <div class="header">
   	<h2>Register</h2>
   </div>
 
-  <form method="post" action="registration.php">
+  <form method="post" action="register.php">
   	<?php include('errors.php'); ?>
-  	<div>
+  	<div class="input-group">
   	  <label>Username</label>
   	  <input type="text" name="username" value="<?php echo $username; ?>">
   	</div>
-  	<div>
+  	<div class="input-group">
   	  <label>Email</label>
   	  <input type="email" name="email" value="<?php echo $email; ?>">
   	</div>
-  	<div>
+  	<div class="input-group">
   	  <label>Password</label>
   	  <input type="password" name="password_1">
   	</div>
-  	<div>
+  	<div class="input-group">
   	  <label>Confirm password</label>
   	  <input type="password" name="password_2">
   	</div>
-  	<div>
+  	<div class="input-group">
   	  <button type="submit" class="btn" name="reg_user">Register</button>
   	</div>
-  	<p>Already a member? <a href="login.php">Sign in</a>	</p>
+  	<p>
+  		Already a member? <a href="login.php">Sign in</a>
+  	</p>
   </form>
 </body>
 </html>
