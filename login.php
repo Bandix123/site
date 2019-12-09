10130f654d52e97a70e6c0c683f48146b266adc8
Bandix123 <56293436+Bandix123@users.noreply.github.com>
2019-12-09 20:01:38 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,29 +1,32 @@
-<?php include('server.php') ?>
+?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
 <head>
-  
-  <link rel="stylesheet" href="login.css">
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-  <div>
+  <div class="header">
   	<h2>Login</h2>
   </div>
 
   <form method="post" action="login.php">
   	<?php include('errors.php'); ?>
-  	<div>
+  	<div class="input-group">
   		<label>Username</label>
   		<input type="text" name="username" >
   	</div>
-  	<div>
+  	<div class="input-group">
   		<label>Password</label>
   		<input type="password" name="password">
   	</div>
-  	<div>
+  	<div class="input-group">
   		<button type="submit" class="btn" name="login_user">Login</button>
   	</div>
-  	<p>Not yet a member? <a href="register.php">Sign up</a></p>
+  	<p>
+  		Not yet a member? <a href="register.php">Sign up</a>
+  	</p>
   </form>
 </body>
 </html>
+
